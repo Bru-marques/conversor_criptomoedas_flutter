6bfbdc3b06ffa18b91a5771852303d8afcb2da88
Bruna Marques <brunamarques@cloudinfo.pt>
2022-04-22 23:00:26 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/conversos_moedas/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.conversos_moedas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/my_app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'pages/moedas_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "MoedasBase",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: MoedasPage(),
+    );
+  }
+}
